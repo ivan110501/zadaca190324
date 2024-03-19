4ed0f5a71dfd435dcf6a8aee484aa65aa0bf1f3f
ivan110501 <ivan.antunovic292@gmail.com>
2024-03-19 15:05:38 +0100

    homework

---
+++ src/pages/contact.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+type CountryType = {
+  flags: {
+    png: string;
+    alt: string;
+  };
+};
+
+const Contact = () => {
+  const [country, setCountry] = useState<CountryType | undefined>(undefined);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    getData(inputValue);
+  };
+
+  const getData = async (countryName: string) => {
+    await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((res: CountryType[]) => {
+        setCountry(res[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData("croatia");
+  }, []);
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter country"
+        />
+        <button onClick={handleButtonClick}>Get the flag</button>
+      </div>
+      <img src={country?.flags.png} alt="" />
+      <div style={{ width: "20rem" }}>{country?.flags.alt}</div>
+    </>
+  );
+};
+export default Contact;
